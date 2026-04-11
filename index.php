5c25f1a32ae97ccde1b4808a05346fb746f1b198
Ubuntu <ubuntu@ip-172-31-11-17.ap-southeast-1.compute.internal>
2026-04-11 13:30:44 +0000

    Initial Docker PHP project

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+echo "Welcome To PHP APP<br>";
+
+// MySQL connection settings (Docker service name is correct)
+$servername = "mysql";
+$username = "root";
+$password = "rootpassword";
+$dbname = "app_db";
+
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+echo "Connected successfully to the database<br>";
+
+// Query the database
+$sql = "SELECT DATABASE()";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    while ($row = $result->fetch_assoc()) {
+        echo "<br>Current database: " . $row["DATABASE()"];
+    }
+} else {
+    echo "<br>No database selected.";
+}
+
+$conn->close();
+?>
