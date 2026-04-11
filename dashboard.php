9d9c222a0032a0c628d8d81a148b87fe95bf4d66
Ubuntu <ubuntu@ip-172-31-11-17.ap-southeast-1.compute.internal>
2026-04-11 15:17:42 +0000

    Added logout feature and session handling

---
+++ dashboard.php
@@ -0,0 +1,14 @@
+<?php
+session_start();
+
+if (!isset($_SESSION['user'])) {
+    header("Location: login.php");
+    exit();
+}
+?>
+
+<h2>Welcome, <?php echo $_SESSION['user']; ?></h2>
+
+<a href="logout.php">
+    <button>Logout</button>
+</a>
